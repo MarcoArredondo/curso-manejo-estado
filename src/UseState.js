aa012a9381c7ac131bd87df970d27b4f9b7a0c9f
Marco Arredondo <marcoarredondoz28@gmail.com>
2022-12-26 17:03:10 -0600

    Dealing with several individual states in function component

--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,22 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 function UseState({name}){
 
-    const [error, setError] = React.useState(true);
+    const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
+    const [value, setValue] = React.useState('');
 
     React.useEffect(() => {
         console.log("Empezando el efecto");
         if(loading){
+            setError(false);
             setTimeout(() => {
                 console.log("Haciendo validacion");
+                if(value !== SECURITY_CODE){
+                    setError(true);
+                }
                 setLoading(false);
                 console.log("Terminando validacion");
             }, 3000);
@@ -17,6 +24,7 @@ function UseState({name}){
         console.log("Terminando el efecto");
     }, [loading]);
 
+    console.log(value);
     return (
         <div>
             <h2>Eliminar {name}</h2>
@@ -30,7 +38,12 @@ function UseState({name}){
                 <p>Cargando...</p>
             )}
 
-            <input placeholder="Código de seguridad"/>
+            <input 
+                onChange={(event) => {
+                    setValue(event.target.value);
+                }} 
+                value={value} 
+                placeholder="Código de seguridad"/>
             <button
                 onClick={() => setLoading(true)}>Comprobar</button>
         </div>
